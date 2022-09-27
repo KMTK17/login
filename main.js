6105f010eca6c1b0deb024d45a61fd077b2bfce8
Denilson <denilsongonzalezberdugo@gmail.com>
2022-09-27 10:34:54 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+let  usuario = 'Kmtk';
+let  contraseña = '123';
+
+
+document.getElementById('ingresar').addEventListener('click', ()=>{
+    
+    const user = document.getElementById('user').value;
+    const pass = document.getElementById('pass').value;
+
+
+if(user == usuario && pass == contraseña){
+
+    location = 'welcome.html';
+
+}else{
+
+    alert('Usuario o contrañesa incorrecto');
+    document.querySelector('#message').style.display = 'block';
+    
+    setTimeout(()=>{document.querySelector('#message').style.display = 'none'},3000);
+
+}
+})
+
+document.getElementById('pass').addEventListener('mouseenter',mostrar);
+
+function mostrar(){
+
+    document.getElementById('pass').type ='text';   
+
+}
+
+document.getElementById('pass').addEventListener('mouseleave',ocultar);
+
+function ocultar(){
+
+    document.getElementById('pass').type ='password';   
+
+}
+
+document.getElementById('remenber').addEventListener('change',function(z){
+    if (z.target.checked == true){
+        alert("Desea que recordemos sus datos")
+    }
+})
+
